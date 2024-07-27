5d108906448a8280a54ed9a25a98949f514c4487
teonateona12 <teona.piranishvili1@gmail.com>
2024-07-27 12:12:26 +0400

    feat: create student types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-import { Button } from "antd";
+"use client";
+import { Student } from "@/types/student";
+import { Table } from "antd";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  return <Button type="primary">Button</Button>;
+  const [students, setStudents] = useState<Student[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  console.log(students);
+  return (
+    <div style={{ padding: "24px", overflowX: "auto" }}>
+      <h1>სტუდენტები</h1>
+    </div>
+  );
 }
---
+++ src/types/student.ts
@@ -0,0 +1,21 @@
+export interface Student {
+  idNumber: string;
+  code: string;
+  name: string;
+  lastName: string;
+  yearOfSubmission: number;
+  graduationYear: number;
+  status: string;
+  school: string;
+  program: string;
+  email: string;
+  voucher: string;
+  grant: string;
+  citizenship: string;
+  dateOfBirth: string;
+  cityOfBirth: string;
+  language: string;
+  course: string;
+  mobility: string;
+  agent: string;
+}
