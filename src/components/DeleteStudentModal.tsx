ced5c99a072b339e6c0358b0d2cb943e0c4d7d35
teonateona12 <teona.piranishvili1@gmail.com>
2024-07-28 16:10:44 +0400

    style: delete student modal

--- src/components/DeleteStudentModal.tsx
+++ src/components/DeleteStudentModal.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal } from "antd";
+import { Modal, Button } from "antd";
 import { toGeorgianUppercase } from "@/utils/column";
 
 interface DeleteStudentModalProps {
@@ -15,15 +15,30 @@ const DeleteStudentModal: React.FC<DeleteStudentModalProps> = ({
 }) => {
   return (
     <Modal
+      title={
+        <span className="modal-title">
+          {toGeorgianUppercase("ნამდვილად გსურთ სტუდენტის წაშლა?")}
+        </span>
+      }
       open={open}
       onOk={onSuccess}
       onCancel={onCancel}
       okText={toGeorgianUppercase("დიახ")}
       cancelText={toGeorgianUppercase("არა")}
       centered
-    >
-      <p>{toGeorgianUppercase("ნამდვილად გსურთ სტუდენტის წაშლა?")}</p>
-    </Modal>
+      footer={[
+        <Button key="ok" onClick={onSuccess} className="custom-ok-button">
+          {toGeorgianUppercase("კი")}
+        </Button>,
+        <Button
+          key="cancel"
+          onClick={onCancel}
+          className="custom-cancel-button"
+        >
+          {toGeorgianUppercase("არა")}
+        </Button>,
+      ]}
+    />
   );
 };
 
